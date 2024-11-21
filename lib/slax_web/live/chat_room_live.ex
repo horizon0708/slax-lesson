fdac38a56949b1f74db0f8218d92d49bfd8dc66a
James Kim <james@vamp.me>
2024-11-21 20:15:52 +0900

    add placeholder modal for new rooms

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -34,6 +34,7 @@ defmodule SlaxWeb.ChatRoomLive do
           <span class="ml-2 leading-none">Add rooms</span>
           <div class="hidden group-focus:block cursor-default absolute top-8 right-2 bg-white border-slate-200 border py-3 rounded-lg">
             <div class="w-full text-left">
+              <.create_new_room_button />
               <div class="hover:bg-sky-600">
                 <div
                   phx-click={JS.navigate(~p"/rooms")}
@@ -197,6 +198,23 @@ defmodule SlaxWeb.ChatRoomLive do
             </.link>
           </div>
         </div>
+      </div>
+    </div>
+    <.modal id="new-room-modal">
+      <.header>New chat room</.header>
+      (Form goes here)
+    </.modal>
+    """
+  end
+
+  defp create_new_room_button(assigns) do
+    ~H"""
+    <div class="hover:bg-sky-600">
+      <div
+        class="cursor-pointer whitespace-nowrap text-gray-800 hover:text-white px-6 py-1 block"
+        phx-click={show_modal("new-room-modal")}
+      >
+        Create a new room
       </div>
     </div>
     """
